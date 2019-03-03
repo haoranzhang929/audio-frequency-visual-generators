a17eb78195e514a5284013a278d7ce6e4fd5ce5d
Haoran Zhang <haoranzhang@icloud.com>
2019-03-03 11:03:37 +0000

    Visualization
    Visualization

--- scripts/main.js
+++ scripts/main.js
@@ -24,6 +24,8 @@ const webAudioUnlock = context => {
 };
 webAudioUnlock(audioCtx);
 
+// TODO: Allow user to upload Audio
+
 let audio = new Audio("./sample/In My Clouds.mp3");
 let audioSrc = audioCtx.createMediaElementSource(audio);
 audioSrc.connect(analyser);
@@ -32,11 +34,95 @@ analyser.connect(audioCtx.destination);
 let bufferSize = analyser.frequencyBinCount;
 let frequencyData = new Float32Array(bufferSize);
 
+// get init frequency data and stores in Float32Array
 analyser.getFloatFrequencyData(frequencyData);
 
 audio.addEventListener("ended", () => {
   audio.currentTime = 0;
 });
+
+let mapsArr = [];
+// TODO: Allow user to set frequencyBinSelection
+let frequencyBinSelection = [0, 100, 256, 300, 400];
+
+for (let i = 0; i < frequencyBinSelection.length; i++) {
+  mapsArr.push(new Map());
+}
+
+Plotly.plot("vizDiv", getData(0, mapsArr, frequencyData), {
+  title: "Audio Frequency Analyser",
+  autosize: true,
+  xaxis: {
+    title: "Seconds",
+    range: [0, Math.round(audio.duration)]
+  },
+  yaxis: {
+    title: "Frequency Data"
+  }
+});
+
+function render() {
+  requestAnimationFrame(render);
+
+  // get real time frequency data and stores in Float32Array
+  analyser.getFloatFrequencyData(frequencyData);
+
+  let currentTime = Math.round(audio.currentTime);
+  let data = getData(currentTime, mapsArr, frequencyData);
+
+  // Reformat Data for Plotly.extendTraces
+  let newData = { x: [], y: [] };
+  let arrs = [];
+
+  for (let i = 0; i < data.length; i++) {
+    newData.x.push([data[i].x[data[i].x.length - 1]]);
+    newData.y.push([data[i].y[data[i].y.length - 1]]);
+    arrs.push(i);
+  }
+
+  Plotly.relayout("vizDiv", {
+    xaxis: {
+      range: [0, Math.round(audio.duration)]
+    },
+    autosize: true
+  });
+
+  Plotly.extendTraces("vizDiv", newData, arrs);
+}
+
+function getData(time, maps, frequencyData) {
+  let data = [];
+
+  maps.forEach((map, index) => {
+    let d = frequencyData[frequencyBinSelection[index]];
+
+    if (map.get(time) === undefined) {
+      map.set(time, d);
+    }
+
+    let x = [];
+    let y = [];
+
+    map.forEach((value, key) => {
+      if (value !== -Infinity) {
+        y.push(value);
+        x.push(key);
+      }
+    });
+
+    let trace = {
+      x,
+      y,
+      type: "scatter",
+      name: `Bin:${frequencyBinSelection[index]}`
+    };
+
+    data.push(trace);
+  });
+  return data;
+}
+
+render();
 
 // TODO: add function to start music
 
