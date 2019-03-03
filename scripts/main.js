5348a5de977533a0738440889996133c3d1d3856
Haoran Zhang <haoranzhang@icloud.com>
2019-03-03 10:53:52 +0000

    Function to resume audio context when disabled
    Function to resume audio context when disabled

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,27 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 2048;
+// TODO: Get fftSize from user Input
+analyser.fftSize = 1024;
+
+const webAudioUnlock = context => {
+  // Audio Context by default is diabled
+  // This Function is to unlock its state by listening to a touch/click event
+  if (context.state === "suspended" && "ontouchstart" in window) {
+    const unlock = () => {
+      context.resume().then(() => {
+        document.body.removeEventListener("touchstart", unlock);
+        document.body.removeEventListener("touchend", unlock);
+        document.body.removeEventListener("click", unlock);
+      });
+    };
+
+    document.body.addEventListener("touchstart", unlock, false);
+    document.body.addEventListener("touchend", unlock, false);
+    document.body.addEventListener("click", unlock, false);
+  }
+};
+webAudioUnlock(audioCtx);
 
 let audio = new Audio("./sample/In My Clouds.mp3");
 let audioSrc = audioCtx.createMediaElementSource(audio);
