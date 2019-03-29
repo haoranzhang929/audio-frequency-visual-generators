f5c588f9e51bf799909d6263fd4ccec3e6bf1bdd
Haoran Zhang <haoranzhang@icloud.com>
2019-03-29 23:08:43 +0000

    Add options and controls
    Add options and controls

--- scripts/main.js
+++ scripts/main.js
@@ -4,162 +4,165 @@ const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const analyser = audioCtx.createAnalyser();
 
 const slider = document.querySelector("#slider");
-const bufferText = document.querySelector(".bufferText");
-bufferText.innerText = slider.value;
 let sliderValue = slider.value * 2;
 analyser.fftSize = sliderValue;
 
-slider.addEventListener("change", () => {
-  audio.pause();
-  audio.currentTime = 0;
-  playBtn.children[0].innerText = "play_arrow";
-  playBtn.children[1].innerText = "Play";
+const input = document.querySelector("#audio-upload");
+const sampleBtn = document.querySelector(".sample-btn");
 
-  bufferText.innerText = slider.value;
-  sliderValue = slider.value * 2;
-  analyser.fftSize = sliderValue;
+// defalut sample track
+sampleBtn.addEventListener("click", () => {
+  init("./sample/In My Clouds.mp3");
 });
 
-const webAudioUnlock = context => {
-  // Audio Context by default is diabled
-  // This Function is to unlock its state by listening to a touch/click event
-  if (context.state === "suspended" && "ontouchstart" in window) {
-    const unlock = () => {
-      context.resume().then(() => {
-        document.body.removeEventListener("touchstart", unlock);
-        document.body.removeEventListener("touchend", unlock);
-        document.body.removeEventListener("click", unlock);
-      });
-    };
-
-    document.body.addEventListener("touchstart", unlock, false);
-    document.body.addEventListener("touchend", unlock, false);
-    document.body.addEventListener("click", unlock, false);
-  }
-};
-webAudioUnlock(audioCtx);
-
-// TODO: Allow user to upload Audio
-let audio = new Audio("./sample/In My Clouds.mp3");
-let audioSrc = audioCtx.createMediaElementSource(audio);
-audioSrc.connect(analyser);
-analyser.connect(audioCtx.destination);
-
-let bufferSize = analyser.frequencyBinCount;
-let frequencyData = new Float32Array(bufferSize);
-
-// get init frequency data and stores in Float32Array
-analyser.getFloatFrequencyData(frequencyData);
-
-audio.addEventListener("ended", () => {
-  audio.currentTime = 0;
-  playBtn.children[0].innerText = "play_arrow";
-  playBtn.children[1].innerText = "Play";
+input.addEventListener("change", e => {
+  audioFilePath = URL.createObjectURL(e.target.files[0]);
+  init(audioFilePath);
 });
 
-let mapsArr = [];
-// TODO: Allow user to set frequencyBinSelection
-let frequencyBinSelection = [0, 100, 256, 300, 400];
+function init(audioFilePath) {
+  // Resume audio context
+  audioCtx.resume();
 
-for (let i = 0; i < frequencyBinSelection.length; i++) {
-  mapsArr.push(new Map());
-}
+  const playBtn = document.querySelector(".play-btn");
 
-Plotly.plot("vizDiv", getData(0, mapsArr, frequencyData), {
-  title: "Audio Frequency Analyser",
-  autosize: true,
-  xaxis: {
-    title: "Seconds",
-    range: [0, Math.round(audio.duration)]
-  },
-  yaxis: {
-    title: "Frequency Data"
-  }
-});
-
-function render() {
-  requestAnimationFrame(render);
-
-  if (audio.paused) {
-    return;
+  if (playBtn.classList.contains("disabled")) {
+    playBtn.classList.remove("disabled");
   }
 
-  // get real time frequency data and stores in Float32Array
+  slider.disabled = false;
+
+  slider.addEventListener("change", () => {
+    audio.pause();
+    audio.currentTime = 0;
+    playBtn.children[0].innerText = "play_arrow";
+    playBtn.children[1].innerText = "Play";
+
+    bufferText.innerText = slider.value;
+    sliderValue = slider.value * 2;
+    analyser.fftSize = sliderValue;
+  });
+
+  let audio = new Audio(audioFilePath);
+  let audioSrc = audioCtx.createMediaElementSource(audio);
+  audioSrc.connect(analyser);
+  analyser.connect(audioCtx.destination);
+
+  let bufferSize = analyser.frequencyBinCount;
+  let frequencyData = new Float32Array(bufferSize);
+
+  // get init frequency data and stores in Float32Array
   analyser.getFloatFrequencyData(frequencyData);
 
-  let currentTime = Math.round(audio.currentTime);
-  let data = getData(currentTime, mapsArr, frequencyData);
+  audio.addEventListener("ended", () => {
+    audio.currentTime = 0;
+    playBtn.children[0].innerText = "play_arrow";
+    playBtn.children[1].innerText = "Play";
+  });
 
-  // Reformat Data for Plotly.extendTraces
-  let newData = { x: [], y: [] };
-  let arrs = [];
+  let mapsArr = [];
+  // TODO: Allow user to set frequencyBinSelection
+  let frequencyBinSelection = [0, 100, 256, 300, 400];
 
-  for (let i = 0; i < data.length; i++) {
-    newData.x.push([data[i].x[data[i].x.length - 1]]);
-    newData.y.push([data[i].y[data[i].y.length - 1]]);
-    arrs.push(i);
+  for (let i = 0; i < frequencyBinSelection.length; i++) {
+    mapsArr.push(new Map());
   }
 
-  drawGraph(newData, arrs);
-}
-
-function drawGraph(data, arrs) {
-  Plotly.relayout("vizDiv", {
+  Plotly.plot("vizDiv", getData(0, mapsArr, frequencyData), {
+    title: "Audio Frequency Analyser",
+    autosize: true,
     xaxis: {
+      title: "Seconds",
       range: [0, Math.round(audio.duration)]
     },
-    autosize: true
+    yaxis: {
+      title: "Frequency Data"
+    }
   });
 
-  Plotly.extendTraces("vizDiv", data, arrs);
-}
+  function render() {
+    requestAnimationFrame(render);
 
-function getData(time, maps, frequencyData) {
-  let data = [];
-
-  maps.forEach((map, index) => {
-    let d = frequencyData[frequencyBinSelection[index]];
-
-    if (map.get(time) === undefined) {
-      map.set(time, d);
+    if (audio.paused) {
+      return;
     }
 
-    let x = [];
-    let y = [];
+    // get real time frequency data and stores in Float32Array
+    analyser.getFloatFrequencyData(frequencyData);
 
-    map.forEach((value, key) => {
-      if (value !== -Infinity) {
-        y.push(value);
-        x.push(key);
-      }
+    let currentTime = Math.round(audio.currentTime);
+    let data = getData(currentTime, mapsArr, frequencyData);
+
+    // Reformat Data for Plotly.extendTraces
+    let newData = { x: [], y: [] };
+    let arrs = [];
+
+    for (let i = 0; i < data.length; i++) {
+      newData.x.push([data[i].x[data[i].x.length - 1]]);
+      newData.y.push([data[i].y[data[i].y.length - 1]]);
+      arrs.push(i);
+    }
+
+    drawGraph(newData, arrs);
+  }
+
+  function drawGraph(data, arrs) {
+    Plotly.relayout("vizDiv", {
+      xaxis: {
+        range: [0, Math.round(audio.duration)]
+      },
+      autosize: true
     });
 
-    let trace = {
-      x,
-      y,
-      type: "scatter",
-      name: `Bin:${frequencyBinSelection[index]}`
-    };
+    Plotly.extendTraces("vizDiv", data, arrs);
+  }
 
-    data.push(trace);
-  });
-  return data;
+  function getData(time, maps, frequencyData) {
+    let data = [];
+
+    maps.forEach((map, index) => {
+      let d = frequencyData[frequencyBinSelection[index]];
+
+      if (map.get(time) === undefined) {
+        map.set(time, d);
+      }
+
+      let x = [];
+      let y = [];
+
+      map.forEach((value, key) => {
+        if (value !== -Infinity) {
+          y.push(value);
+          x.push(key);
+        }
+      });
+
+      let trace = {
+        x,
+        y,
+        type: "scatter",
+        name: `Bin:${frequencyBinSelection[index]}`
+      };
+
+      data.push(trace);
+    });
+    return data;
+  }
+
+  render();
+
+  audio.onloadedmetadata = () => {
+    playBtn.addEventListener("click", () => {
+      if (audio.paused) {
+        audio.play();
+        playBtn.children[0].innerText = "pause";
+        playBtn.children[1].innerText = "Pause";
+        slider.disabled = true;
+      } else {
+        audio.pause();
+        playBtn.children[0].innerText = "play_arrow";
+        playBtn.children[1].innerText = "Play";
+      }
+    });
+  };
 }
-
-render();
-
-const playBtn = document.querySelector(".play-btn");
-
-audio.onloadedmetadata = () => {
-  playBtn.addEventListener("click", () => {
-    if (audio.paused) {
-      audio.play();
-      playBtn.children[0].innerText = "pause";
-      playBtn.children[1].innerText = "Pause";
-    } else {
-      audio.pause();
-      playBtn.children[0].innerText = "play_arrow";
-      playBtn.children[1].innerText = "Play";
-    }
-  });
-};
